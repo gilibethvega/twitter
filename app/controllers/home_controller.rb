6b6deea34effe187342594dfb5fabcf5bc72df02
Gilibeth Vega <gvega@creditu.com>
2021-03-04 20:26:00 -0300

    se hace vista para seguir a amigos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
   end
   def users
+    @users = User.all
     if current_user.present?
       @following = current_user.users_followed
     else
